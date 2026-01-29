d11a0b4d4a12f141a89a44ad63b510be9fc068f7
Lukas Axelsson <lukasaxelsson@Mac.localdomain>
2026-01-29 12:01:55 +0400

    Initial SwiftUI app setup

---
+++ Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "HackathonLukasClient",
+    platforms: [.macOS(.v14)],
+    targets: [
+        .executableTarget(
+            name: "HackathonLukasClient",
+            path: "Sources"
+        ),
+    ]
+)
